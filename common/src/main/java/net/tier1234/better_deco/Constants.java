c33f6a06598de37b7886f7f3465c2799b5f622df
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-04-11 15:34:12 +0200

    Project Bases

--- common/src/main/java/net/tier1234/better_deco/Constants.java
+++ common/src/main/java/net/tier1234/better_deco/Constants.java
@@ -1,5 +1,6 @@
 package net.tier1234.better_deco;
 
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,4 +9,8 @@ public class Constants {
     public static final String MOD_ID = "better_deco";
     public static final String MOD_NAME = "BetterDeco";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+
+    public static ResourceLocation id(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+    }
 }
