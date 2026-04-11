b0e5d5b571db8c8566de020f13ea583601dcee90
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-04-11 15:33:34 +0200

    Setting Up

---
+++ common/src/main/java/net/tier1234/better_deco/Constants.java
@@ -0,0 +1,11 @@
+package net.tier1234.better_deco;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "better_deco";
+    public static final String MOD_NAME = "BetterDeco";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
