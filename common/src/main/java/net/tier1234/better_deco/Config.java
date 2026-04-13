172c1914847b9e2937e2d4f6a35dc27c86b99566
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-04-13 17:21:20 +0200

    Adding Configs

--- common/src/main/java/net/tier1234/better_deco/Config.java
+++ common/src/main/java/net/tier1234/better_deco/Config.java
@@ -1,7 +1,6 @@
 package net.tier1234.better_deco;
 
-import com.mrcrayfish.framework.api.config.ConfigType;
-import com.mrcrayfish.framework.api.config.FrameworkConfig;
+import com.mrcrayfish.framework.api.config.*;
 
 public class Config {
 
@@ -10,6 +9,14 @@ public class Config {
 
     public static class Client {
 
+        @ConfigProperty(name = "Sink Allow All Liquids", comment = """
+                Define if the sink accept all liquids: by default it accepts only water, turning this config to true,
+                allow the sink to accept lava or modded fluids""")
+        public final BoolProperty sinkAllowAllLiquids = BoolProperty.create(false);
+
+        @ConfigProperty(name = "Sink Capacity", comment = """
+                Define the maximun capcacity of sinks in buckets""")
+        public final IntProperty sinkCapacity = IntProperty.create(3,1,10);
 
     }
 }
