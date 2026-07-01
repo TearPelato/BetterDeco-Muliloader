abbe93333c275fca0a1830de8d7718053dc5edd1
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-07-02 01:25:03 +0200

    Cutout render fix on fabric

--- fabric/src/main/java/net/tier1234/better_deco/util/CutoutRenderLayerBlocks.java
+++ fabric/src/main/java/net/tier1234/better_deco/util/CutoutRenderLayerBlocks.java
@@ -1,13 +1,51 @@
 package net.tier1234.better_deco.util;
 
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import net.tier1234.better_deco.init.ModBlocks;
 
 public class CutoutRenderLayerBlocks {
 
 
-    public static TagKey<Block> getBlocks() {
-        return BlockTags.BAMBOO_BLOCKS;
+    public static Block[] getBlocks() {
+        return new Block[]{
+                ModBlocks.OAK_SINK.get(),
+                ModBlocks.SPRUCE_SINK.get(),
+                ModBlocks.BIRCH_SINK.get(),
+                ModBlocks.JUNGLE_SINK.get(),
+                ModBlocks.ACACIA_SINK.get(),
+                ModBlocks.DARK_OAK_SINK.get(),
+                ModBlocks.MANGROVE_SINK.get(),
+                ModBlocks.CHERRY_SINK.get(),
+                ModBlocks.BAMBOO_SINK.get(),
+                ModBlocks.CRIMSON_SINK.get(),
+                ModBlocks.WARPED_SINK.get(),
+
+                ModBlocks.OAK_GLASS_TECQUE.get(),
+                ModBlocks.SPRUCE_GLASS_TECQUE.get(),
+                ModBlocks.BIRCH_GLASS_TECQUE.get(),
+                ModBlocks.JUNGLE_GLASS_TECQUE.get(),
+                ModBlocks.ACACIA_GLASS_TECQUE.get(),
+                ModBlocks.DARK_OAK_GLASS_TECQUE.get(),
+                ModBlocks.MANGROVE_GLASS_TECQUE.get(),
+                ModBlocks.CHERRY_GLASS_TECQUE.get(),
+                ModBlocks.BAMBOO_GLASS_TECQUE.get(),
+                ModBlocks.CRIMSON_GLASS_TECQUE.get(),
+                ModBlocks.WARPED_GLASS_TECQUE.get(),
+                ModBlocks.STONE_GLASS_TECQUE.get(),
+
+                ModBlocks.OAK_OVEN.get(),
+                ModBlocks.SPRUCE_OVEN.get(),
+                ModBlocks.BIRCH_OVEN.get(),
+                ModBlocks.JUNGLE_OVEN.get(),
+                ModBlocks.ACACIA_OVEN.get(),
+                ModBlocks.DARK_OAK_OVEN.get(),
+                ModBlocks.MANGROVE_OVEN.get(),
+                ModBlocks.CHERRY_OVEN.get(),
+                ModBlocks.BAMBOO_OVEN.get(),
+                ModBlocks.CRIMSON_OVEN.get(),
+                ModBlocks.WARPED_OVEN.get()
+
+
+        };
     }
 }
